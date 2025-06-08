e90aab7fd380ad7eaae0b123809e17819d9fb37c
sadiiq111 <Sadiiqarab1@gmail.com>
2025-06-08 00:06:29 -0700

    Add files via upload

--- DailySpecailsExtra.java
+++ DailySpecailsExtra.java
@@ -4,13 +4,10 @@ public class DailySpecailsExtra {
 
 
     public static void main (String[] args) {
-
+        Scanner input = new Scanner(System.in);
         String specials;
 
-        Scanner input = new Scanner(System.in);
-
         System.out.println("Please enter a day of the week excluding weekends (Monday - Friday only!");
-
         specials = input.next();
 
 
@@ -30,15 +27,17 @@ public class DailySpecailsExtra {
             sunday = specials.equalsIgnoreCase("Sunday");
         }
 
+      specials = specials.toLowerCase();
+
         switch (specials) {
             // my case will be the day of the week
-            case "Monday":
+            case "monday":
                 coffee = "Latte";
                 price = 4.95;
                 break;
 
 
-            case "Tuesday":
+            case "tuesday":
                 coffee = "Frapp";
                 price = 5.95;
 
@@ -51,13 +50,13 @@ public class DailySpecailsExtra {
                 break;
 
 
-            case "Thursday":
+            case "thursday":
                 coffee = "Regular Joe";
                 price = 2.95;
 
                 break;
 
-            case "Friday":
+            case "friday":
                 coffee = "Green Tea Latte";
                 price = 3.95;
                 break;
@@ -69,21 +68,21 @@ public class DailySpecailsExtra {
         System.out.println(specials + "'s coffee of the day is a  " +  coffee + "  and the price will be $ " +price);
         System.out.println(" How many " + coffee + "s would you like to order?");
         quantity = input.nextInt();
-
-
+    double total = quantity * price;
         if(quantity == 0) {
             System.out.println("Looks like you don't like " + coffee + ", so sad!");
 
-        }else if (quantity ==1) {
+        }else if (quantity == 1) {
             System.out.println("Looks like you will be ordering only 1 " + coffee + " today!");
-        }else if ( quantity >=2 && quantity < 9){
+        }else if ( quantity >=2 && quantity <= 9){
             System.out.println(quantity + " " + coffee+ " Have been ordered and totaling");
             System.out.println(price+ " dollars ! thank you for your order");
-        } else if (quantity <15 && quantity >=10 ) {
+        } else if (quantity >=10 && quantity <=15) {
             System.out.println(quantity + " " + coffee + " have been ordered totaling ");
-            System.out.printf("$%.2f dollars", (quantity * price));
+            System.out.printf(" Total: $%.2f dollars. ", total);
+            System.out.println("Thank you for your big order!");
         }else {
-          System.out.println(" thank you for your order");
+          System.out.println(" thank you for your order.");
         }
 
 
